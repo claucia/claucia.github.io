fdb34a01695992e17faa87fd9389480aacbee319
Claucia Cambruzzi <cambruzziclaucia@gmail.com>
2022-01-17 00:15:20 +0000

    Enhance header

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
-console.log("Hello, world!");
+const hamburger = document.querySelector(".hamburguer");
+const navigationMenu = document.querySelector(".navigation-menu");
+const navigationLink = document.querySelectorAll(".navigation-link");
+
+// Events
+hamburger.addEventListener("click", mobileMenu);
+navigationLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+// Functions
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navigationMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navigationMenu.classList.remove("active");
+}
