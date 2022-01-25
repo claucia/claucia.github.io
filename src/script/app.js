523ffc4b428d66e19c5d40b4e6a9ecbdd8d3647d
Claucia Cambruzzi <cambruzziclaucia@gmail.com>
2022-01-25 16:37:37 +0000

    Add scroll to top function

--- src/script/app.js
+++ src/script/app.js
@@ -1,3 +1,4 @@
 import "./contact-form.js";
 import "./footer-copyright.js";
 import "./mobile-menu.js";
+import "./scroll-to-top.js";
---
+++ src/script/scroll-to-top.js
@@ -0,0 +1,4 @@
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+document.getElementById("scroll-to-top").addEventListener("click", scrollToTop);
